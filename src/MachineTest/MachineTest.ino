fa9e873252a7180c6fa2975318a28b8914218db4
akkdong <akkdong@infobank.net>
2017-10-13 16:57:45 +0900

    pin assign

--- src/MachineTest/MachineTest.ino
+++ src/MachineTest/MachineTest.ino
@@ -1,11 +1,21 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+
+#include <libmaple/libmaple.h>
+#include <libmaple/gpio.h>
+#include <libmaple/timer.h>
+#include <libmaple/usart.h>
+
+DEFINE_HWSERIAL(serial, 1);
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  while(! Serial);
+  serial.begin(115200);
+  while(! serial);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello...");
+  serial.println("Hello...");
   delay(1);
 }
