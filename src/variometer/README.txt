1622c2413fca6fb0f81aa67fc36cd5bdd56677a3
akkdong <akkdong@infobank.net>
2017-10-16 17:01:33 +0900

    start of work

---
+++ src/variometer/README.txt
@@ -0,0 +1,1 @@
+Set Arduio SkechBook path to here
---
+++ src/variometer/skeches/Variometer/Variometer.ino
@@ -0,0 +1,29 @@
+// Variometer.ino
+//
+
+#include <Arduino.h>
+#include <VarioSettings.h>
+#include <VertVelocity.h>
+
+//#include <IMU.h>
+
+
+
+VertVelocity  vertVel;
+//IMU				imu;
+
+void setup()
+{
+	//
+	
+	//
+	// imu.init();
+	
+	// imu.update()
+	// imu.get();
+	// ...
+}
+
+void loop()
+{
+}
