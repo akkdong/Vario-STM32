f090607b967ef880610ce5ae2f4c314a34bc8cb9
Kangwoo Cheon <akkdong@nate.com>
2018-02-15 14:27:19 +0900

    add another example

--- src/variometer/sketches/Variometer/Jump2Bootloader.cpp
+++ src/variometer/sketches/Variometer/Jump2Bootloader.cpp
@@ -82,3 +82,44 @@ void JumpToBootloader(void) {
      *       and test with bootloader works with STM32 Flash Loader Demonstrator software
      */
 }
+
+
+
+
+#if 0
+// another example : it uses HAL driver
+// ref : https://community.st.com/thread/40238-stm32l476rg-jump-to-bootloader-from-software
+
+	typedef  void (*pFunction)(void);
+	pFunction JumpToApplication;
+	uint32_t JumpAddress;
+
+	HAL_RCC_DeInit();
+
+	SysTick->CTRL = 0;
+	SysTick->LOAD = 0;
+	SysTick->VAL = 0;
+
+
+	/**
+	 * Step: Disable all interrupts
+	 */
+	__disable_irq();
+
+	/* ARM Cortex-M Programming Guide to Memory Barrier Instructions.*/
+	__DSB();
+		   
+	__HAL_SYSCFG_REMAPMEMORY_SYSTEMFLASH();
+
+	/* Remap is bot visible at once. Execute some unrelated command! */
+	__DSB();
+	__ISB();
+
+	JumpToApplication = (void (*)(void)) (*((uint32_t *)(0x1FFF0000 + 4)));
+			  
+	/* Initialize user application's Stack Pointer */
+	__set_MSP(*(__IO uint32_t*) 0x1FFF0000);
+
+	JumpToApplication();
+
+#endif
